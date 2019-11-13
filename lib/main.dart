b14edab3b570ac2bdca3a424175737b9546006b0
pandadevoper <the1ime@yandex.ru>
2019-11-13 06:44:40 +0500

    Init

---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'StartUp Name Generator',
+      home: RandomWords(),
+    );
+  }
+}
+
+class RandomWordsState extends State<RandomWords>{
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  @override
+  Widget build (BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('StartUp Name Generator')
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions(){
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i){
+        if (i.isOdd) return Divider();
+
+        final index = i ~/ 2;
+        if (index >= _suggestions.length){
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(index+1, _suggestions[index]);
+      },
+    );
+  }
+
+  Widget _buildRow (int index, WordPair wordPair){
+    return ListTile(
+      leading: Text(index.toString()), 
+      title: Text(
+        wordPair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => RandomWordsState();
+}
